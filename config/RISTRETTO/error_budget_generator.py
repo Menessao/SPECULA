fa18ba7f1d1a5327b333c5755e6fa06d08c5b718
Menessao <menessinimatteo@gmail.com>
2026-06-06 15:38:08 +0200

    Updated notebooks and plots

--- config/RISTRETTO/error_budget_generator.py
+++ config/RISTRETTO/error_budget_generator.py
@@ -84,6 +84,7 @@ for i,n_subap in enumerate(n_subaps):
                             f"seeing_random.constant: {seeing:1.2f}, "
                             f"dm_perfect.nmodes: {N:1.0f}, "
                             f"modal_analysis.nmodes: {N:1.0f}, "
+                            f"source_ngs.magnitude: 5.0, "
                             f"pyr_slopes.pupdata_object: 'pyr_pupdata_{n_subap:.0f}x{n_subap:.0f}', "
                             f"pyr_sn.output_tag: 'pyr{rMod:1.1f}_{n_subap:.0f}x{n_subap:.0f}_s{seeing:1.2f}_{N:1.0f}modes_sn', "
                             f"data_store.store_dir:         {os.path.join(root_dir,'scratch_aliasing')}, "  
@@ -104,49 +105,49 @@ for i,n_subap in enumerate(n_subaps):
                     fits.writeto(os.path.join(aliaspath,tag+'_alias.fits'),alias_rms,overwrite=True)
                     print('Saved aliasing power as: '+tag+'_alias')
                     frames = fits.getdata(os.path.join(root_dir,'scratch_aliasing','pyr_frames.fits'))
-                    frames_avg = np.mean(frames,axis=0) 
+                    frames_avg = np.mean(frames,axis=0) #np.sqrt(np.sum(frames**2,axis=0)) #
                     fits.writeto(os.path.join(framespath,tag+'_avg_frame.fits'),frames_avg,overwrite=True)
                     print('Saved average frame as: '+tag+'_avg_frame')
 
 
-# 4. Calibrate SIMPC vs n_subap, rMods, seeing for PERFECT correction
-ncycles = 50
-fs = 2000
-for i,n_subap in enumerate(n_subaps):
-    pup_dist = np.max((min_pup_dist,max_pup_dist/max(n_subaps)*n_subap))
-    for rMod in rMods:
-        for seeing in seeings:
-            for N in n_modes[:np.minimum(i+1,len(n_modes))]:
-                tag = f'pyr{rMod:1.1f}_{n_subap:.0f}x{n_subap:.0f}_s{seeing:1.2f}_{N:1.0f}modes'
-                simpc_tag = tag+'_simpc'
-                overrides = ("{"
-                            f"main.total_time: {N*2*ncycles/fs}, "
-                            f"atmo_random.update_interval: {N*2:1.0f}, "
-                            f"pyr.pup_diam: {n_subap:.1f}, "
-                            f"pyr.pup_dist: {pup_dist:.1f}, "
-                            f"pyr.mod_amp: {rMod:.1f}, "
-                            f"pushpull.nmodes: {N:1.0f}, "
-                            f"pushpull.ncycles: {ncycles:1.0f}, "
-                            f"pyr_im_calibrator.nmodes: {N:1.0f}, "
-                            f"dm_perfect.nmodes: {N:1.0f}, "
-                            f"dm.nmodes: {N:1.0f}, "
-                            f"pyr_slopes.pupdata_object: 'pyr_pupdata_{n_subap:.0f}x{n_subap:.0f}', "
-                            f"seeing_random.constant: {seeing:1.2f}, "
-                            f"pyr_im_calibrator.im_tag: '{simpc_tag}', "
-                            # f"data_store.store_dir:         '{os.path.join(root_dir,'scratch_simpc')}', "  
-                            # f"data_store.create_tn: false, "
-                            # f"data_store.inputs.input_list: ['{N:1.0f}modes_pushpull-pushpull.output'], " 
-                            "}")
-                write_yaml_overrides(input_string=overrides)
-                try:
-                    os.system(f"specula {main_config} calib_simpc.yml temp_overrides.yml")
-                    # simpc = fits.getdata(os.path.join(root_dir,'im',simpc_tag+'.fits'))
-                    # og = np.diag(simpc.T @ im)/im_norm
-                    # cog = np.sqrt(np.diag(simpc.T @ simpc)/im_norm - og**2)
-                    # fits.writeto(os.path.join(ogpath,tag+'_og_pl.fits'),og)
-                    # print('Saved optical gains as: '+tag+'_og_pl')
-                    # fits.writeto(os.path.join(ogpath,tag+'_compl_og_pl.fits'),cog)
-                    # print('Saved complementary (perpedicular) optical gains as: '+tag+'_compl_og_pl')
-                except FileExistsError: #OSError:
-                    pass
+# # 4. Calibrate SIMPC vs n_subap, rMods, seeing for PERFECT correction
+# ncycles = 50
+# fs = 2000
+# for i,n_subap in enumerate(n_subaps):
+#     pup_dist = np.max((min_pup_dist,max_pup_dist/max(n_subaps)*n_subap))
+#     for rMod in rMods:
+#         for seeing in seeings:
+#             for N in n_modes[:np.minimum(i+1,len(n_modes))]:
+#                 tag = f'pyr{rMod:1.1f}_{n_subap:.0f}x{n_subap:.0f}_s{seeing:1.2f}_{N:1.0f}modes'
+#                 simpc_tag = tag+'_simpc'
+#                 overrides = ("{"
+#                             f"main.total_time: {N*2*ncycles/fs}, "
+#                             f"atmo_random.update_interval: {N*2:1.0f}, "
+#                             f"pyr.pup_diam: {n_subap:.1f}, "
+#                             f"pyr.pup_dist: {pup_dist:.1f}, "
+#                             f"pyr.mod_amp: {rMod:.1f}, "
+#                             f"pushpull.nmodes: {N:1.0f}, "
+#                             f"pushpull.ncycles: {ncycles:1.0f}, "
+#                             f"pyr_im_calibrator.nmodes: {N:1.0f}, "
+#                             f"dm_perfect.nmodes: {N:1.0f}, "
+#                             f"dm.nmodes: {N:1.0f}, "
+#                             f"pyr_slopes.pupdata_object: 'pyr_pupdata_{n_subap:.0f}x{n_subap:.0f}', "
+#                             f"seeing_random.constant: {seeing:1.2f}, "
+#                             f"pyr_im_calibrator.im_tag: '{simpc_tag}', "
+#                             # f"data_store.store_dir:         '{os.path.join(root_dir,'scratch_simpc')}', "  
+#                             # f"data_store.create_tn: false, "
+#                             # f"data_store.inputs.input_list: ['{N:1.0f}modes_pushpull-pushpull.output'], " 
+#                             "}")
+#                 write_yaml_overrides(input_string=overrides)
+#                 try:
+#                     os.system(f"specula {main_config} calib_simpc.yml temp_overrides.yml")
+#                     # simpc = fits.getdata(os.path.join(root_dir,'im',simpc_tag+'.fits'))
+#                     # og = np.diag(simpc.T @ im)/im_norm
+#                     # cog = np.sqrt(np.diag(simpc.T @ simpc)/im_norm - og**2)
+#                     # fits.writeto(os.path.join(ogpath,tag+'_og_pl.fits'),og)
+#                     # print('Saved optical gains as: '+tag+'_og_pl')
+#                     # fits.writeto(os.path.join(ogpath,tag+'_compl_og_pl.fits'),cog)
+#                     # print('Saved complementary (perpedicular) optical gains as: '+tag+'_compl_og_pl')
+#                 except FileExistsError: #OSError:
+#                     pass
 
