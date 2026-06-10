271bd67b3dbd48c48619d4d9e2ba7d7d30ef5ae6
Menessao <menessinimatteo@gmail.com>
2026-06-10 02:16:57 +0200

    EOD commit

--- config/ErrorBudget/synim.py
+++ config/ErrorBudget/synim.py
@@ -1,8 +1,16 @@
 from astropy.io import fits
 import numpy as np
+import os
+import pandas as pd
+
+import specula 
+specula.init(0)
+
 from scipy.ndimage import rotate
 from specula.data_objects.ifunc import IFunc
-from specula.data_objects.ifunc_inv import IFuncInv
+
+# from specula.data_objects.ifunc_inv import IFuncInv
+# from scipy.ndimage import zoom
 
 klinv = fits.getdata('/raid1/mmenessini/calibration/SOUL/KLv30dx/ifunc/asm_v30dx_kl_inv.fits')
 kl = np.linalg.pinv(klinv)
@@ -14,26 +22,76 @@ pyr_masks = fits.getdata('/raid1/mmenessini/calibration/SOUL/KLv30dx/pupils/lbt_
 
 filepath=f'/raid1/mmenessini/calibration/SOUL/KLv30dx/pupils/lbt_pupdata.fits'
 
+
+# def change_magnification(image, factor):
+#     scaled = zoom(image, factor, order=3)
+#     old_h, old_w = image.shape
+#     new_h, new_w = scaled.shape
+#     if factor >= 1.0:
+#         start_y = (new_h - old_h) // 2
+#         start_x = (new_w - old_w) // 2
+#         return scaled[start_y:start_y + old_h, start_x:start_x + old_w]
+#     else:
+#         output = np.zeros_like(image)
+#         start_y = (old_h - new_h) // 2
+#         start_x = (old_w - new_w) // 2
+#         output[start_y:start_y + new_h, start_x:start_x + new_w] = scaled
+#         return output
+
+def shift_image(image, shift, axis):
+    shift_int = int(np.floor(shift))
+    shift_frac = shift - shift_int
+    def integer_shift(img, pixels, ax):
+        if pixels == 0:
+            return img.copy()
+        pad_width = [(0, 0), (0, 0)]
+        if pixels > 0:
+            pad_width[ax] = (pixels, 0)  # Pad start (left/top)
+            sliced_img = np.pad(img, pad_width, mode='constant', constant_values=0)
+            if ax == 0: return sliced_img[:-pixels, :]
+            else:       return sliced_img[:, :-pixels]
+        else:
+            pad_width[ax] = (0, abs(pixels))  # Pad end (right/bottom)
+            sliced_img = np.pad(img, pad_width, mode='constant', constant_values=0)
+            if ax == 0: return sliced_img[abs(pixels):, :]
+            else:       return sliced_img[:, abs(pixels):]
+    img_floor = integer_shift(image, shift_int, axis)
+    img_ceil = integer_shift(image, shift_int + 1, axis)
+    shifted_image = img_floor * (1.0 - shift_frac) + img_ceil * shift_frac
+    return shifted_image
+
+# def rotate_ifunc_inv(rot_deg:float):
+#     kl_new = np.zeros_like(kl)
+#     img = np.zeros(lbtpup.shape)
+#     for j in range(kl.shape[1]):
+#         img[lbtpup.astype(bool)] = kl[:,j]
+#         rot_img = rotate(img,angle=rot_deg,reshape=False)
+#         kl_new[:,j] = rot_img[lbtpup.astype(bool)]
+#     kl_inv_new = np.linalg.pinv(kl_new)
+#     ifunc_inv_obj = IFuncInv(ifunc_inv=kl_inv_new, mask=lbtpup)
+#     ifunc_inv_obj.save('/raid1/mmenessini/calibration/SOUL/KLv30dx/ifunc/asm_v30dx_ifunc_optshift_inv.fits', overwrite=True)
+
 def rotate_ifunc(rot_deg:float):
     ifunc_new = np.zeros_like(ifunc)
-    kl_new = np.zeros_like(kl)
     img = np.zeros(lbtpup.shape)
     for j in range(ifunc.shape[1]):
         img[lbtpup.astype(bool)] = ifunc[:,j]
         rot_img = rotate(img,angle=rot_deg,reshape=False)
         ifunc_new[:,j] = rot_img[lbtpup.astype(bool)]
     ifunc_obj = IFunc(ifunc=ifunc_new.T,mask=lbtpup)
-    ifunc_obj.save('/raid1/mmenessini/calibration/SOUL/KLv30dx/ifunc/asm_v30dx_ifunc_shift.fits', overwrite=True)
-    for j in range(kl.shape[1]):
-        img[lbtpup.astype(bool)] = kl[:,j]
-        rot_img = rotate(img,angle=rot,reshape=False)
-        kl_new[:,j] = rot_img[lbtpup.astype(bool)]
-    kl_inv_new = np.linalg.pinv(kl_new)
-    ifunc_inv_obj = IFuncInv(ifunc_inv=kl_inv_new, mask=lbtpup)
-    ifunc_inv_obj.save('/raid1/mmenessini/calibration/SOUL/KLv30dx/ifunc/asm_v30dx_ifunc_shift_inv.fits', overwrite=True)
+    ifunc_obj.save('/raid1/mmenessini/calibration/SOUL/KLv30dx/ifunc/asm_v30dx_ifunc_optshift.fits', overwrite=True)
+
+def shift_ifunc(shift:float,ax_dir):
+    ifunc_new = np.zeros_like(ifunc)
+    img = np.zeros(lbtpup.shape)
+    for j in range(ifunc.shape[1]):
+        img[lbtpup.astype(bool)] = ifunc[:,j]
+        shift_img = shift_image(img,shift=shift,axis=ax_dir)
+        ifunc_new[:,j] = shift_img[lbtpup.astype(bool)]
+    ifunc_obj = IFunc(ifunc=ifunc_new.T,mask=lbtpup)
+    ifunc_obj.save('/raid1/mmenessini/calibration/SOUL/KLv30dx/ifunc/asm_v30dx_ifunc_optshift.fits', overwrite=True)
 
 Nslopes = 2512
-Nmodes = 400
 npix = 120
 
 half_mask = pyr_masks[:60,:120].astype(bool)
@@ -41,19 +99,16 @@ pup_hdu = fits.open(filepath)
 pup_ids = pup_hdu[1].data
 fimg = np.zeros(npix**2)
 
-
-def evaluate_error():
+def evaluate_error(Nmodes:int):
     refim = im[:Nslopes,:Nmodes]
-    aux = fits.getdata('/raid1/mmenessini/calibration/SOUL/KLv30dx/im/pyr3.0_40x40_lbt_synim.fits')
+    aux = fits.getdata('/raid1/mmenessini/calibration/SOUL/KLv30dx/im/pyr3.0_40x40_lbt_optsynim.fits')[:,:Nmodes]
     synim = aux.copy()
     synim[:Nslopes//2,:] = aux[Nslopes//2:,:]
     synim[Nslopes//2:,:] = aux[:Nslopes//2,:]*-1
-
     synim -= np.mean(synim,axis=0)
     synim *= np.std(refim,axis=0)/np.std(synim,axis=0)
-
     err = np.zeros(Nmodes)
-    for j in Nmodes:
+    for j in range(Nmodes):
         img = np.zeros(np.size(half_mask))
         img[half_mask.flatten()] = refim[pupids,j]
         img = img.reshape([60,120])
@@ -61,5 +116,66 @@ def evaluate_error():
         np.put(fimg, pup_ids[:,1], synim[Nslopes//2:,j])
         f2d = fimg.reshape([npix,npix])
         delta = img - f2d[:60,:120]
-        err[j] = np.sum(delta[half_mask]**2)
+        err[j] = np.sqrt(np.sum(delta[half_mask]**2))
     return err
+
+def evaluate_metric(Nmodes):
+    main_config = 'syn_soul_im.yml'
+    os.system(f"specula {main_config}")
+    err = evaluate_error(Nmodes)
+    metric = np.sqrt(np.sum(err**2))
+    return metric
+
+
+
+if __name__ == "__main__":
+
+    Nmodes = 500
+
+    rot0 = 52.5
+    shiftX0 = 0.0
+    shiftY0 = 0.0
+
+    rotvec = np.linspace(-2,2,15)
+    shiftvec = np.linspace(-1,1,21)
+
+    result_dir = '/raid1/mmenessini/results/SOUL/KLv30dx/'
+
+    columns = ['rotation','shiftX','shiftY','magnification','metric']
+    result = []
+
+    print('Setting default parameters')
+    shift_ifunc(shiftX0,ax_dir=0)
+    shift_ifunc(shiftY0,ax_dir=1)
+
+    for rot in rotvec:
+        print(f'Testing rotation: {rot}')
+        rotate_ifunc(rot_deg=rot+rot0)
+        chi = evaluate_metric(Nmodes)
+        result.append({'rotation': rot+rot0, 'shiftX': shiftX0, 'shiftY': shiftY0, 'magnification': 1.00, 'metric': chi})
+        print(f'Obtained metric: {chi}')
+    rotate_ifunc(rot_deg=rot0)
+
+    for shft in shiftvec:
+        print(f'Testing x-shift: {shft}')
+        shift_ifunc(shft+shiftX0,ax_dir=0)
+        chi = evaluate_metric(Nmodes)
+        result.append({'rotation': rot0, 'shiftX': shft+shiftX0, 'shiftY': shiftY0, 'magnification': 1.00, 'metric': chi})
+        print(f'Obtained metric: {chi}')
+    shift_ifunc(shiftX0,ax_dir=0)
+
+    for shft in shiftvec:
+        print(f'Testing y-shift: {shft}')
+        shift_ifunc(shft+shiftY0,ax_dir=1)
+        chi = evaluate_metric(Nmodes)
+        result.append({'rotation': rot0, 'shiftX': shiftX0, 'shiftY': shft+shiftY0, 'magnification': 1.00, 'metric': chi})
+        print(f'Obtained metric: {chi}')
+    shift_ifunc(shiftY0,ax_dir=1)
+
+    results_df = pd.DataFrame(result, columns=columns) 
+    results_df.to_csv(os.path.join(result_dir, 'misreg_csv', f'deltaRot{np.max(abs(rotvec)):1.2f}_deltaShift{np.max(abs(shiftvec)):1.2f}_metric.csv'), index=False)
+        
+
+
+                
+
--- specula/mmlib/compute_lbt_rec.py
+++ specula/mmlib/compute_lbt_rec.py
@@ -3,13 +3,32 @@ from astropy.io import fits
 import numpy as np
 import os.path as op
 
+
+pyr_masks = fits.getdata('/raid1/mmenessini/calibration/SOUL/KLv30dx/pupils/lbt_pupmask_shift.fits').astype(bool)
+pupids = fits.getdata('/raid1/mmenessini/calibration/SOUL/KLv30dx/pupils/pup_ids.fits')
+filepath=f'/raid1/mmenessini/calibration/SOUL/KLv30dx/pupils/lbt_pupdata.fits'
+rec_hdr = fits.getheader('/raid1/mmenessini/calibration/SOUL/KLv30dx/data/Rec_LUCI2_IIR_bin1_500modes.fits')
+npix = 120
+pup_hdu = fits.open(filepath)
+pup_ids = pup_hdu[1].data
+fimg = np.zeros(npix**2)
+
 def generate_rec(im,Nmodes:int,argos:bool,iir_path:str='/raid1/mmenessini/calibration/SOUL/KLv30dx/data/iir_rows.fits'):
+    aux = np.zeros_like(im)
     IM = np.zeros_like(im)
     # Adjust slopes
-    IM[:2512//2,:] = im[2512//2:,:]
-    IM[2512//2:,:] = im[:2512//2,:]*-1
+    aux[:2512//2,:] = im[2512//2:,:]
+    aux[2512//2:,:] = im[:2512//2,:]*-1
     # Adjust scaling
-    IM *= 4e+9
+    aux *= 4e+9
+    half_mask = pyr_masks[:60,:120].astype(bool)
+    img = np.zeros(np.size(half_mask))
+    for i in range(aux.shape[1]):
+        np.put(fimg, pup_ids[:,0], aux[:2512//2,i])
+        np.put(fimg, pup_ids[:,1], aux[2512//2:,i])
+        f2d = fimg.reshape([npix,npix])
+        img = f2d[:60,:]
+        IM[pupids,i] = img.flatten()[half_mask.flatten()]
     iir_rows = fits.getdata(iir_path)
     pad_iir_rows = np.pad(iir_rows,pad_width=((600-Nmodes,0),(0,0)),mode='constant',constant_values=0.0)
     IMinv = np.linalg.pinv(IM[:2512,:Nmodes])
@@ -18,17 +37,19 @@ def generate_rec(im,Nmodes:int,argos:bool,iir_path:str='/raid1/mmenessini/calibr
     Rec = Rec.astype('>f4')
     if argos:
         Rec /= 2
-    return Rec
+    return Rec,IM
 
 
 if __name__ == "__main__":
     impath = '/raid1/mmenessini/calibration/SOUL/KLv30dx/im'
     recpath = '/raid1/mmenessini/calibration/SOUL/KLv30dx/rec'
-    tag = 'pyr3.0_40x40_shift'
-    Nmodes = np.array([200,300,400,500])
+    tag = 'pyr3.0_s1.0_synim' #'pyr3.0_40x40_shift_im' #'
+    Nmodes = np.array([100]) #np.array([100,200,300,400,500])
     print(tag)
-    IM = fits.getdata(op.join(impath,tag+'_im.fits'))
+    IntMat = fits.getdata(op.join(impath,tag+'.fits'))
     for N in Nmodes:
-        Rec = generate_rec(IM,N,argos=True)
-        fits.writeto(op.join(recpath,tag+'_rec.fits'),Rec,overwrite=True)
+        Rec,IM = generate_rec(IntMat,N,argos=True)
+        fits.writeto(op.join(recpath,tag+f'_lbtlike_im.fits'),IM,header=rec_hdr,overwrite=True)
+        print(f'Saved im as {tag}_lbtlike_im.fits')
+        fits.writeto(op.join(recpath,tag+f'_{N}modes_rec.fits'),Rec,header=rec_hdr,overwrite=True)
         print(f'Saved rec as {tag}_{N}modes_rec.fits')
