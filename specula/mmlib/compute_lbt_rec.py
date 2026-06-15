aa9c83153d7da52f89729898b904eff5e5bd6c4e
Menessao <menessinimatteo@gmail.com>
2026-06-15 18:42:28 +0200

    EOD commit

--- specula/mmlib/compute_lbt_rec.py
+++ specula/mmlib/compute_lbt_rec.py
@@ -30,10 +30,10 @@ def generate_rec(im,Nmodes:int,argos:bool): #,iir_path:str='/raid1/mmenessini/ca
         img = f2d[:60,:]
         IM[pupids,i] = img.flatten()[half_mask.flatten()]
     IMinv = np.linalg.pinv(IM[:2512,:Nmodes])
-    Rec = np.pad(IMinv,pad_width=((672-Nmodes,0),(0,2848-2512)),mode='constant',constant_values=0.0)
+    Rec = np.pad(IMinv,pad_width=((0,672-Nmodes),(0,2848-2512)),mode='constant',constant_values=0.0)
     # Add IIR rows
-    Rec[661,:] = IMinv[0,:].copy()    
-    Rec[668,:] = IMinv[1,:].copy()
+    Rec[661,:] = np.pad(IMinv[0,:],pad_width=(0,2848-2512),mode='constant',constant_values=0.0)   
+    Rec[668,:] = np.pad(IMinv[1,:],pad_width=(0,2848-2512),mode='constant',constant_values=0.0)  
     Rec = Rec.astype('>f4')
     if argos:
         Rec /= 2
@@ -43,13 +43,15 @@ def generate_rec(im,Nmodes:int,argos:bool): #,iir_path:str='/raid1/mmenessini/ca
 if __name__ == "__main__":
     impath = '/raid1/mmenessini/calibration/SOUL/KLv30dx/im'
     recpath = '/raid1/mmenessini/calibration/SOUL/KLv30dx/rec'
-    tag = 'pyr3.0_s1.0_synim' #'pyr3.0_40x40_shift_im' #'
-    Nmodes = np.array([100,200,300,400,500,600])
+    tag = 'pyr3.0_40x40_lbt_optsynim' # 'pyr3.0_s1.0_synim_pcinf' # 'pyr3.0_s1.0_synim_pcperf' # 
+    Nmodes = np.array([100,200,300,400,500,550,600])
     print(tag)
     IntMat = fits.getdata(op.join(impath,tag+'.fits'))
+    imtag = 'pyr3.0_s1.0_dl_synth'
+    rectag = 'pyr3.0_s1.0_dl_synth'
     for N in Nmodes:
         Rec,IM = generate_rec(IntMat,N,argos=True)
-        fits.writeto(op.join(recpath,tag+f'_lbtlike_im.fits'),IM,header=rec_hdr,overwrite=True)
-        print(f'Saved im as {tag}_lbtlike_im.fits')
-        fits.writeto(op.join(recpath,tag+f'_{N}modes_rec.fits'),Rec,header=rec_hdr,overwrite=True)
-        print(f'Saved rec as {tag}_{N}modes_rec.fits')
+        fits.writeto(op.join(recpath,imtag+f'_lbtlike_im.fits'),IM,header=rec_hdr,overwrite=True)
+        print(f'Saved im as {imtag}_lbtlike_im.fits')
+        fits.writeto(op.join(recpath,rectag+f'_{N}modes_rec.fits'),Rec,header=rec_hdr,overwrite=True)
+        print(f'Saved rec as {rectag}_{N}modes_rec.fits')
